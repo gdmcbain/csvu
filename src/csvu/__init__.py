dbb68ecd2b5fbfa2083933e24abcbaf21ffa4946
G. D. McBain <gdmcbain@pm.me>
2025-03-25 16:13:41 +1100

    write parenthetically by default too

--- src/csvu/__init__.py
+++ src/csvu/__init__.py
@@ -9,12 +9,12 @@ import pint_pandas
 
 def parse_raw_name_algebraic(name: str, delimiter: str = "/") -> Tuple[str, str]:
     quantity = name.split(delimiter, 1)
-    return quantity[0], (quantity[1].strip(" ()") if quantity[1:] else "dimensionless")
+    return quantity[0].rstrip(), (quantity[1].strip(" ()") if quantity[1:] else "dimensionless")
 
 
 def parse_raw_name_bracketed(name: str, brackets: str = "()") -> Tuple[str, str]:
     quantity = name.split(brackets[0], 1)
-    return quantity[0], (
+    return quantity[0].rstrip(), (
         quantity[1].rstrip(f"{brackets[1]} ") if quantity[1:] else "dimensionless"
     )
 
@@ -34,12 +34,12 @@ def read_csv(
     return df.pint.quantify()
 
 
-def write_csv(df: pd.DataFrame, filepath: Path) -> None:
+def write_csv(df: pd.DataFrame, filepath: Path, fmt: str = "{} ({})") -> None:
     def prettify_units(t) -> str:
         s = f"{t.units}"
-        return f"({s})" if " " in s else s
+        return f"{s}" if " " in s else s
 
-    columns = {c: f"{c} / {prettify_units(df.dtypes[c])}" for c in df.columns}
+    columns = {c: fmt.format(c, prettify_units(df.dtypes[c])) for c in df.columns}
     df.astype(float).rename(columns=columns).set_index(columns[df.columns[0]]).to_csv(
         filepath
     )
